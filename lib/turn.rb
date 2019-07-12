89cf2a7cf162b8ac828d6a0ebdfdee15e28e0552
Amy Fong <amyhfong@gmail.com>
2019-07-12 19:35:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  (index.between?(0,8) && !position_taken?(board, index))?
+  (index.between?(0,8) && !position_taken?(board, index))
 end
 
 def input_to_index(input)
